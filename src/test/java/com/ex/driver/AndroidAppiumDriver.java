8304537aaa67565b0ef849d5306ccabfd06f3882
Sean Chandler <sean.chandler@gmail.com>
2017-02-17 19:00:06 -0800

    added <MobileElement> to driver, removed old app

--- src/test/java/com/ex/driver/AndroidAppiumDriver.java
+++ src/test/java/com/ex/driver/AndroidAppiumDriver.java
@@ -3,6 +3,7 @@ package com.ex.driver;
 import com.ex.appium.Appium;
 import com.ex.capabilities.CapabilitiesFactory;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 /**
@@ -11,6 +12,6 @@ import io.appium.java_client.android.AndroidDriver;
 public class AndroidAppiumDriver implements Driver {
     @Override
     public AppiumDriver getDriver() {
-        return new AndroidDriver(Appium.getUrl(), CapabilitiesFactory.getCapabilities().getCapabilities());
+        return new AndroidDriver<MobileElement>(Appium.getUrl(), CapabilitiesFactory.getCapabilities().getCapabilities());
     }
 }
--- src/test/java/com/ex/driver/IOSAppiumDriver.java
+++ src/test/java/com/ex/driver/IOSAppiumDriver.java
@@ -3,6 +3,7 @@ package com.ex.driver;
 import com.ex.appium.Appium;
 import com.ex.capabilities.CapabilitiesFactory;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
 
 /**
@@ -11,6 +12,6 @@ import io.appium.java_client.ios.IOSDriver;
 public class IOSAppiumDriver implements Driver {
     @Override
     public AppiumDriver getDriver() {
-        return new IOSDriver(Appium.getUrl(), CapabilitiesFactory.getCapabilities().getCapabilities());
+        return new IOSDriver<MobileElement>(Appium.getUrl(), CapabilitiesFactory.getCapabilities().getCapabilities());
     }
 }
